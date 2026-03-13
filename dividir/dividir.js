da91cb3da6063ff4638ddaea0e8288818d059f24
Alexsandro-J-Ludwig <alexsandrojacksomludwigvasco@gmail.com>
2026-03-12 21:18:22 -0300

    feat: divisao de numeradores

---
+++ dividir/dividir.js
@@ -0,0 +1,16 @@
+const numerador = 1;
+let divisor = 0;
+let x = 0;
+
+function dividir() {
+    if (divisor === 0 && numerador > 1) {
+        return "Não é divisivel por 0";
+    } else if (dividir === 0 && numerador === 1) {
+        return x = 1;
+    } else {
+        return numerador / divisor;
+    }
+}
+
+const resultado = dividir()
+console.log(resultado);
