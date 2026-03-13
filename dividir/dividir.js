58a1177f0920d2338e151663ed4e788588dfa0b9
Alexsandro-J-Ludwig <alexsandrojacksomludwigvasco@gmail.com>
2026-03-12 21:39:30 -0300

    fix: divisio 1/0 = infinity removed

--- dividir/dividir.js
+++ dividir/dividir.js
@@ -3,10 +3,8 @@ let divisor = 0;
 let x = 0;
 
 function dividir() {
-    if (divisor === 0 && numerador > 1) {
+    if (divisor === 0) {
         return "Não é divisivel por 0";
-    } else if (dividir === 0 && numerador === 1) {
-        return x = 1;
     } else {
         return numerador / divisor;
     }
